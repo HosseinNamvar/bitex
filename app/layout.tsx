45907b8cbb40c5612585e2fcc6f40b11e270fdaa
Hossein Namvar <hossein_nam@yahoo.com>
2024-01-07 23:34:02 +0100

    Added: Outfit font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import localFont from "next/font/local";
+const Outfit = localFont({ src: "../public/fonts/Outfit-VariableFont.ttf" });
 
 export const metadata: Metadata = {
   title: "BITEX",
@@ -16,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Outfit.className}>{children}</body>
     </html>
   );
 }
