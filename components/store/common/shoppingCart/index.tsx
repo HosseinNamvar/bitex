5d75576228649a0db6a3e33fcf9cfcac6da61146
Hossein Namvar <hossein_nam@yahoo.com>
2024-01-23 15:21:44 +0100

    style: shopping cart animation added

--- components/store/common/shoppingCart/index.tsx
+++ components/store/common/shoppingCart/index.tsx
@@ -15,7 +15,7 @@ const ShoppingCart = ({ isVisible, quantity = 0, handleOnClose }: IProps) => {
       className={`${styles.shoppingCart} ${!isVisible && styles.shoppingHide}`}
     >
       <div className={styles.background} onClick={handleOnClose} />
-      <div className={styles.cartWindow}>
+      <div className={`${styles.cartWindow} ${isVisible && styles.showWindow}`}>
         <div className={styles.header}>
           <h2>Shopping Cart ({quantity})</h2>
           <button onClick={handleOnClose}>
