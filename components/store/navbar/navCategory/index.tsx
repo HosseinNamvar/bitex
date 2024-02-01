7adb1d9c782f3b4b52a323a8ddd2ad1c665719c3
Hossein <hossein_nam@yahoo.com>
2024-02-01 17:28:13 +0100

    fix: toggel menu issue when clicking on multi buttons

--- components/store/navbar/navCategory/index.tsx
+++ components/store/navbar/navCategory/index.tsx
@@ -1,10 +1,11 @@
-"use client";
-import React, { useRef } from "react";
+"use-client";
 import styles from "./navCategory.module.scss";
-import { ListIcon } from "@/components/icons/svgIcons";
-import { CategoriesData } from "@/data/categories";
+
+import { useRef } from "react";
 import Link from "next/link";
 import { useToggleMenu } from "@/hooks/useToggleMenu";
+import { ListIcon } from "@/components/icons/svgIcons";
+import { CategoriesData } from "@/data/categories";
 
 const NavBarCategory = () => {
   const dropdownRef = useRef<HTMLDivElement>(null);
--- hooks/useToggleMenu.ts
+++ hooks/useToggleMenu.ts
@@ -2,27 +2,32 @@ import { useEffect, useState } from "react";
 
 export const useToggleMenu = (
   initialState: boolean,
-  refElement: React.RefObject<HTMLDivElement>
+  refElement: React.RefObject<HTMLDivElement>,
+  forceToggle?: boolean // if true when click on same element it will not toggle
 ) => {
   const [isActive, setIsActive] = useState<boolean>(initialState);
 
   useEffect(() => {
     const toggle = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        refElement.current &&
-        !refElement.current.contains(e.target)
-      ) {
+      if (forceToggle) {
+        if (
+          e.target instanceof HTMLElement &&
+          refElement.current &&
+          !refElement.current.contains(e.target)
+        ) {
+          setIsActive(!isActive);
+        }
+      } else {
         setIsActive(!isActive);
       }
     };
 
     if (isActive) {
-      window.addEventListener("click", toggle);
+      document.addEventListener("click", toggle);
     }
 
     return () => {
-      window.removeEventListener("click", toggle);
+      document.removeEventListener("click", toggle);
     };
   }, [isActive, refElement]);
 
