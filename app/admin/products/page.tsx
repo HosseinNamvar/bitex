dfa1757280864822f826c1c8158bcb510cd4313e
Hossein <hossein_nam@yahoo.com>
2024-02-06 12:52:39 +0100

    feat: admin add product HTML added

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,14 +1,34 @@
 "use client";
 import Button from "@/components/UI/button";
 import styles from "./adminProducts.module.scss";
+import RadioButton from "@/components/UI/radioButton";
+import { useState } from "react";
+import DropDownList from "@/components/UI/dropDown";
+import { TDropDown } from "@/types/uiElements";
+import { CategoriesData } from "@/data/categories";
 
 const AdminProducts = () => {
+  const [showProductWindow, setShowProductWindow] = useState(false);
+  const category: TDropDown = {
+    selectedIndex: 0,
+    options: [
+      {
+        text: "text01",
+        value: 0,
+      },
+      {
+        text: "text02",
+        value: 1,
+      },
+    ],
+  };
+
   return (
     <div className={styles.adminProducts}>
       <div className={styles.header}>
         <Button
           text="Add new product"
-          onClick={() => console.log("add product")}
+          onClick={() => setShowProductWindow(true)}
         />
       </div>
       <div className={styles.dataTable}>
@@ -57,6 +77,111 @@ const AdminProducts = () => {
           </div>
         </div>
       </div>
+      {showProductWindow ? (
+        <div className={styles.addProduct}>
+          <div
+            className={styles.background}
+            onClick={() => setShowProductWindow(false)}
+          />
+          <div className={styles.productsWindow}>
+            <div className={styles.header}>Add Product</div>
+            <div className={styles.nameAndCat}>
+              <div>
+                <span>Name:</span>
+                <input type="text" placeholder="Name..." />
+              </div>
+              <div>
+                <span>Short Descriptions:</span>
+                <input type="text" placeholder="Short Description..." />
+              </div>
+              <div>
+                <span>Category</span>
+                <DropDownList
+                  data={category}
+                  onChange={() => console.log("")}
+                />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.col1}>On the sale:</span>
+              <div className={styles.col2}>
+                <RadioButton id="1" value="Yes" groupName="isSale" />
+                <RadioButton id="2" value="No" groupName="isSale" />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.col1}></span>
+              <div className={styles.col2}>
+                <span>Duration:</span>
+                <input type="text" />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.col1}></span>
+              <div className={styles.col2}>
+                <span>Sale Price:</span>
+                <input type="number" />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.col1}>Options</span>
+              <div className={styles.col3}>
+                <span>Colors:</span>
+                <div>
+                  <input type="checkbox" />
+                  <span>Red</span>
+                </div>
+                <div>
+                  <input type="checkbox" />
+                  <span>Blue</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.col1}></span>
+              <div className={styles.col3}>
+                <span>Storage Capacity:</span>
+                <div>
+                  <input type="checkbox" />
+                  <span>128GB</span>
+                </div>
+                <div>
+                  <input type="checkbox" />
+                  <span>256GB</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.col1}>Specifications:</span>
+              <div className={styles.col3}>
+                <span>Overall</span>
+                <div>
+                  <span>Dimension</span>
+                  <input type="text" />
+                  <Button onClick={() => console.log("")} text="+" />
+                </div>
+                <div>
+                  <span>Special Features</span>
+                  <input type="text" />
+                  <Button onClick={() => console.log("")} text="+" />
+                </div>
+                <span>Cameras</span>
+                <div>
+                  <span>Front Camera</span>
+                  <input type="text" />
+                  <Button onClick={() => console.log("")} text="+" />
+                </div>
+              </div>
+            </div>
+            <div className={styles.windowControl}>
+              <Button text="cancel" onClick={() => console.log("")} />
+              <Button text="Add" onClick={() => console.log("")} />
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
