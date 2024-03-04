ed968849729d72944514dff8e92556bff8512f66
Hossein <hossein_nam@yahoo.com>
2024-03-04 15:06:32 +0100

    fix: fixing showing correct day and month in Admin traffic view

--- app/admin/trafficView/page.tsx
+++ app/admin/trafficView/page.tsx
@@ -5,9 +5,9 @@ const convertTime = (time: Date) => {
   return (
     time.getFullYear() +
     "." +
-    time.getMonth().toLocaleString("en-us", { minimumIntegerDigits: 2 }) +
+    (time.getMonth() + 1).toLocaleString("en-us", { minimumIntegerDigits: 2 }) +
     "." +
-    time.getDay().toLocaleString("en-us", { minimumIntegerDigits: 2 }) +
+    time.getDate().toLocaleString("en-us", { minimumIntegerDigits: 2 }) +
     "  --  " +
     time.getHours().toLocaleString("en-us", { minimumIntegerDigits: 2 }) +
     ":" +
@@ -19,6 +19,7 @@ const convertTime = (time: Date) => {
 
 const TrafficView = async () => {
   const response = await getTrafficReport();
+  console.log(response.res);
   return (
     <div className={styles.trafficView}>
       {response.res &&
