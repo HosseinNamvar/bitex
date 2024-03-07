8d7bcfc89e7d59638cffb1f5d315511fb1047b6b
Hossein <hossein_nam@yahoo.com>
2024-03-07 16:41:39 +0100

    feat: warning and contact message added

---
+++ components/store/common/warning/index.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import styles from "./warning.module.scss";
+import { LinkedinIcon } from "@/components/icons/svgIcons";
+
+const Warning = () => {
+  return (
+    <div className={styles.warning}>
+      <div className={styles.wrapper}>
+        <span>
+          This website is a personal endeavor created for portfolio purposes and
+          is not a real business.
+        </span>
+        <div className={styles.contact}>
+          <span>Contact: </span>
+          <Link
+            href={"https://www.linkedin.com/in/hossein-namvar/"}
+            title="My LinkedIn Profile"
+            target="_blank"
+          >
+            <LinkedinIcon width={20} strokeWidth={0} />
+            LinkedIn
+          </Link>
+          <Link
+            href={"mailto:hossein.namvar@gmail.com"}
+            title="Send Me an Email!"
+          >
+            Email
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Warning;
