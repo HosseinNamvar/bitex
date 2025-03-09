a42486e52db70cfa5625a89299f1030d956ff62f
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-09 22:50:39 +0100

    chore: tailwind init and fonts moved to assets folder

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["var(--font-outfit)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
