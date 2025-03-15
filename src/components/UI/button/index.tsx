728891b90aa87fc1ab5e0006ec40c2cb29a88c5e
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-15 21:28:14 +0100

    refactor: sass replaced with tailwind in button component

--- src/components/UI/button/index.tsx
+++ src/components/UI/button/index.tsx
@@ -1,27 +1,27 @@
 "use client";
-import styles from "./button.module.scss";
 
-interface IProps {
-  text: string;
+import { cn } from "@/shared/utils/styling";
+
+type TProps = {
   type?: "button" | "reset" | "submit";
   disabled?: boolean;
   onClick?: () => void;
-}
+  className?: string;
+  children?: React.ReactNode;
+};
 
-const Button = ({
-  text,
-  onClick,
-  type = "button",
-  disabled = false,
-}: IProps) => {
+const Button = ({ onClick, type = "button", disabled = false, className, children }: TProps) => {
   return (
     <button
       disabled={disabled}
       type={type}
-      className={styles.button}
+      className={cn(
+        "w-full flex cursor-pointer bg-white border border-gray-300 items-center justify-center px-4 py-2 transition-all gap-4 duration-300 text-gray-700 rounded-lg disabled:cursor-default disabled:bg-gray-100",
+        className
+      )}
       onClick={onClick}
     >
-      {text}
+      {children}
     </button>
   );
 };
