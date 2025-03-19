6d6dde865f188143f1cce9f66e1b4c248daa2697
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-20 00:18:43 +0100

    refactor: sass replaced with tailwind in wideAd component

--- src/components/store/home/wideAd/index.tsx
+++ src/components/store/home/wideAd/index.tsx
@@ -1,35 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/shared/utils/styling";
 
-import styles from "./wideAd.module.scss";
-
-interface IProps {
+type TProps = {
   imgUrl: string;
   linkText?: string;
   url: string;
   title: string;
   isLightBG?: boolean;
   smallTitle: string;
-}
+};
 
-const WideAd = ({
-  imgUrl,
-  linkText = "Show Deals",
-  smallTitle,
-  title,
-  url,
-  isLightBG = false,
-}: IProps) => {
+const WideAd = ({ imgUrl, linkText = "Show Deals", smallTitle, title, url, isLightBG = false }: TProps) => {
   return (
     <div
-      className={`${styles.wideAd} ${
-        isLightBG ? styles.darkText : styles.lightText
-      }`}
+      className={cn("relative text-white h-[200px] flex-grow pl-7", isLightBG ? "styles.darkText" : "styles.lightText")}
     >
-      <span>{smallTitle}</span>
-      <h3>{title}</h3>
-      <Link href={url}>{linkText}</Link>
-      <Image src={imgUrl} fill alt={title} sizes="(max-width:440px)" />
+      <span className="relative z-5 text-sm mt-10 block">{smallTitle}</span>
+      <h3 className="relative z-5 text-lg font-medium block mt-2 mb-12">{title}</h3>
+      <Link href={url} className="relative z-5">
+        {linkText}
+      </Link>
+      <Image
+        src={imgUrl}
+        className="z-1 absolute rounded-xl w-full h-full object-cover"
+        fill
+        alt={title}
+        sizes="(max-width:440px)"
+      />
     </div>
   );
 };
