24853ffa5ab6ddfcc9ca95cfb88fd4618d15df82
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-24 21:03:41 +0100

    style: wide card style fixed

--- src/components/store/home/wideAd/index.tsx
+++ src/components/store/home/wideAd/index.tsx
@@ -13,11 +13,9 @@ type TProps = {
 
 const WideAd = ({ imgUrl, linkText = "Show Deals", smallTitle, title, url, isLightBG = false }: TProps) => {
   return (
-    <div
-      className={cn("relative text-white h-[200px] flex-grow pl-7", isLightBG ? "styles.darkText" : "styles.lightText")}
-    >
-      <span className="relative z-5 text-sm mt-10 block">{smallTitle}</span>
-      <h3 className="relative z-5 text-lg font-medium block mt-2 mb-12">{title}</h3>
+    <div className={cn("relative h-[200px] flex-grow pl-7", isLightBG ? "text-gray-900" : "text-gray-100")}>
+      <span className="relative z-5 text-sm mt-8 block">{smallTitle}</span>
+      <h3 className="relative z-5 text-lg font-medium block mt-2 mb-16">{title}</h3>
       <Link href={url} className="relative z-5">
         {linkText}
       </Link>
