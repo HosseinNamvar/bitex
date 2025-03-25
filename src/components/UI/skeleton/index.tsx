78e3a58e1b399f89ba913cb5795741a6561dc2c9
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-25 23:19:01 +0100

    improve: sass replaced with tailwind and animation improved

--- src/components/UI/skeleton/index.tsx
+++ src/components/UI/skeleton/index.tsx
@@ -1,19 +1,21 @@
-import styles from "./skeleton.module.scss";
-
-interface IProps {
-  width: string;
-  height?: string;
-}
-
-export const SK_Box = ({ width, height }: IProps) => {
-  return <div className={styles.skeletonBase} style={{ width, height }} />;
+type TProps = {
+  width?: string | number;
+  height?: string | number;
+  rounded?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "full";
+  className?: string;
 };
 
-export const SK_Circle = ({ width }: IProps) => {
+export const SK_Box = ({ width = "100%", height = "1rem", rounded = "sm", className = "" }: TProps) => {
   return (
     <div
-      className={`${styles.skeletonBase} ${styles.circle}`}
-      style={{ width, height: width }}
+      className={`
+        relative inline-block overflow-hidden bg-gray-200
+        after:absolute after:inset-0
+        after:bg-[linear-gradient(90deg,rgba(255,255,255,0)_0%,rgba(255,255,255,0.5)_50%,rgba(255,255,255,0)_100%)]
+        after:animate-[loading_1.5s_infinite]
+        rounded-${rounded} ${className}
+      `}
+      style={{ width, height }}
     />
   );
 };
