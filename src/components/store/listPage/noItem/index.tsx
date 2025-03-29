52d5e2b7ec8dfdf70f3bfd62588f08c79f809c80
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-30 00:37:12 +0100

    style: sass replaced with tailwind in no item component

--- src/components/store/listPage/noItem/index.tsx
+++ src/components/store/listPage/noItem/index.tsx
@@ -1,21 +1,44 @@
 import Link from "next/link";
-import styles from "./noItem.module.scss";
 
-interface IProps {
+type TProps = {
   pageHeader: string;
-}
+};
 
-const NoItem = ({ pageHeader }: IProps) => {
+const NoItem = ({ pageHeader }: TProps) => {
   return (
-    <div className={styles.noItemContainer}>
+    <div
+      className={
+        "styles.noItemContainer w-full h-full min-h-[600px] flex flex-col gap-16 text-gray-800 items-center mt-16 text-lg font-light"
+      }
+    >
       <span> There is no product in {pageHeader} category!</span>
-      <div>
-        <span> You Can Check These Categories Instead:</span>
-        <div>
-          <Link href={"/list/pc-laptops/computer"}>Computers</Link>
-          <Link href={"/list/pc-laptops/laptops"}>Laptop</Link>
-          <Link href={"/list/smartphones"}>Mobile</Link>
-          <Link href={"/list/tablets"}>Tablet</Link>
+      <div className="text-sm flex flex-col gap-4">
+        <span className="w-full text-center"> You Can Check These Categories Instead:</span>
+        <div className="flex gap-3 items-center justify-center">
+          <Link
+            className="border border-gray-300 px-4 py-1 rounded-md transition-colors duration-300 hover:bg-gray-100"
+            href={"/list/pc-laptops/computer"}
+          >
+            Computers
+          </Link>
+          <Link
+            className="border border-gray-300 px-4 py-1 rounded-md transition-colors duration-300 hover:bg-gray-100"
+            href={"/list/pc-laptops/laptops"}
+          >
+            Laptop
+          </Link>
+          <Link
+            className="border border-gray-300 px-4 py-1 rounded-md transition-colors duration-300 hover:bg-gray-100"
+            href={"/list/smartphones"}
+          >
+            Mobile
+          </Link>
+          <Link
+            className="border border-gray-300 px-4 py-1 rounded-md transition-colors duration-300 hover:bg-gray-100"
+            href={"/list/tablets"}
+          >
+            Tablet
+          </Link>
         </div>
       </div>
     </div>
