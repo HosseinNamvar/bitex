12f1ea03221e37fb2ec1b1c966bf863f6c5df4b2
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-30 16:10:17 +0200

    refactor: sass replaced with tailwind

--- src/components/admin/category/rowGroup/categoryOptions/AddOption/index.tsx
+++ src/components/admin/category/rowGroup/categoryOptions/AddOption/index.tsx
@@ -1,18 +1,18 @@
 "use client";
-import styles from "./addOption.module.scss";
 
 import { useState } from "react";
 
 import Button from "@/components/UI/button";
 import { addOptionSet } from "@/actions/category/categoryOptions";
 import { TOptionSet } from "@/types/common";
+import Input from "@/components/UI/input";
 
-interface IProps {
+type TProps = {
   categoryOptionId: string;
   reloadRequest: () => void;
-}
+};
 
-const AddOption = ({ categoryOptionId, reloadRequest }: IProps) => {
+const AddOption = ({ categoryOptionId, reloadRequest }: TProps) => {
   const [isColor, setIsColor] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [name, setName] = useState("");
@@ -41,30 +41,31 @@ const AddOption = ({ categoryOptionId, reloadRequest }: IProps) => {
   };
 
   return (
-    <div className={styles.addOption}>
-      <div>
-        <span>Name:</span>
-        <input
-          type="text"
-          onChange={(e) => setName(e.currentTarget.value)}
-          value={name}
-          disabled={isLoading}
-        />
+    <div className="w-full flex items-center justify-between rounded-[8px] bg-gray-100 p-1.5 my-[6px] relative">
+      <div className="flex items-center">
+        <span className="mx-2">Name:</span>
+        <Input type="text" onChange={(e) => setName(e.currentTarget.value)} value={name} disabled={isLoading} />
       </div>
-      <div>
-        <span>type:</span>
-        <option disabled={!isColor} onClick={() => setIsColor(false)}>
+      <div className="flex items-center">
+        <span className="mr-2">type:</span>
+        <option
+          className="cursor-pointer rounded-md select-none px-4 py-1.5 transition-all duration-300 mr-1 bg-white border border-gray-300 hover:bg-gray-200 disabled:cursor-default disabled:text-white disabled:bg-blue-500"
+          disabled={!isColor}
+          onClick={() => setIsColor(false)}
+        >
           Text
         </option>
-        <option disabled={isColor} onClick={() => setIsColor(true)}>
+        <option
+          className="cursor-pointer rounded-md select-none px-4 py-1.5 transition-all duration-300 mr-1 bg-white border border-gray-300 hover:bg-gray-200 disabled:cursor-default disabled:text-white disabled:bg-blue-500"
+          disabled={isColor}
+          onClick={() => setIsColor(true)}
+        >
           Color
         </option>
       </div>
-      <Button
-        text="Add Option"
-        disabled={isLoading}
-        onClick={() => handleAddOption()}
-      />
+      <Button className="w-[160px]" disabled={isLoading} onClick={() => handleAddOption()}>
+        Add Option
+      </Button>
     </div>
   );
 };
