1f191b8c7184ee239961c8408f0693fc416afc55
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-30 17:37:50 +0200

    style: button component updated

--- src/components/UI/button/index.tsx
+++ src/components/UI/button/index.tsx
@@ -2,21 +2,27 @@
 
 import { cn } from "@/shared/utils/styling";
 
-type TProps = {
+type TProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   type?: "button" | "reset" | "submit";
-  disabled?: boolean;
-  onClick?: () => void;
-  className?: string;
+  size?: "sm" | "md" | "base" | "lg";
   children?: React.ReactNode;
 };
 
-const Button = ({ onClick, type = "button", disabled = false, className, children }: TProps) => {
+const Button = ({ onClick, size = "md", type = "button", disabled = false, className, children }: TProps) => {
+  const sizeClasses = {
+    sm: "px-2 py-1 text-xs rounded-sm",
+    md: "px-4 py-1 text-sm rounded-md",
+    base: "px-5 py-2 text-base rounded-lg",
+    lg: "px-6 py-2 text-lg rounded-lg",
+  };
+
   return (
     <button
       disabled={disabled}
       type={type}
       className={cn(
-        "w-full flex cursor-pointer bg-white border border-gray-300 items-center justify-center px-4 py-2 transition-all gap-4 duration-300 text-gray-700 rounded-lg disabled:cursor-default disabled:bg-gray-100",
+        "flex cursor-pointer bg-white border border-gray-300 items-center justify-center px-4 py-2 transition-all gap-4 duration-300 text-gray-700 rounded-lg disabled:cursor-default hover:bg-gray-100 active:bg-gray-200 disabled:bg-gray-100",
+        sizeClasses[size],
         className
       )}
       onClick={onClick}
