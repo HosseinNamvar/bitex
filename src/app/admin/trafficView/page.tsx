4f4a224f853da56e0100ffb543a62c4f922299fb
HosseinNamvar <hossein_nam@yahoo.com>
2025-03-30 17:37:08 +0200

    sass replaced with tailwind in traffic view page

--- src/app/admin/trafficView/page.tsx
+++ src/app/admin/trafficView/page.tsx
@@ -1,13 +1,8 @@
 "use client";
-import styles from "./trafficView.module.scss";
 
 import Button from "@/components/UI/button";
 
-import {
-  TTrafficListItem,
-  deleteTraffic,
-  getTrafficReport,
-} from "@/actions/pageVisit/pageVisitServices";
+import { TTrafficListItem, deleteTraffic, getTrafficReport } from "@/actions/pageVisit/pageVisitServices";
 import { useEffect, useState } from "react";
 
 const convertTime = (time: Date) => {
@@ -52,29 +47,27 @@ const TrafficView = () => {
   };
 
   return (
-    <div className={styles.trafficView}>
-      {trafficList.length > 0 ? (
+    <div className="text-sm text-gray-800 flex flex-col overflow-y-scroll">
+      {!!trafficList.length ? (
         trafficList.map((item) => (
-          <div key={item.id} className={styles.row}>
-            <div className={styles.leftCol}>
-              <div className={styles.time}>
-                {item.time ? convertTime(item.time) : ""}
-              </div>
-              <div className={styles.pageType}>{item.pageType}</div>
-              <div className={styles.pagePath}>{item.pagePath}</div>
-              <div className={styles.deviceRes}>{item.deviceResolution}</div>
-              <div>
-                {item.product &&
-                  item.product?.category.name + " / " + item.product?.name}
-              </div>
+          <div
+            key={item.id}
+            className="grid grid-cols-12 items-center justify-between gap-4 p-3 rounded-lg even:bg-gray-100"
+          >
+            <span className="w-[180px] col-span-2 text-center py-1 bg-white rounded-md border border-gray-400">
+              {item.time ? convertTime(item.time) : ""}
+            </span>
+            <span className="w-[160px] col-span-2 text-center py-1 text-gray-600 rounded-md border border-gray-300">
+              {item.pageType}
+            </span>
+            <span className="col-span-2 text-nowrap overflow-hidden">{item.pagePath}</span>
+            <div className="w-[110px] col-span-2 text-center py-1 rounded-md border border-gray-400">
+              {item.deviceResolution}
             </div>
-            <div className={styles.rightCol}>
-              <Button
-                text="Delete"
-                onClick={() => handleDeleteTraffic(item.id)}
-                disabled={isLoading}
-              />
-            </div>
+            <div className="col-span-3">{item.product && item.product?.category.name + " / " + item.product?.name}</div>
+            <Button onClick={() => handleDeleteTraffic(item.id)} disabled={isLoading}>
+              Delete
+            </Button>
           </div>
         ))
       ) : (
