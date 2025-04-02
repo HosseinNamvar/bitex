fa14b7e2655f934fe78a429926be1a69b2cf0d52
HosseinNamvar <hossein_nam@yahoo.com>
2025-04-02 23:31:24 +0200

    feat: pagination component added

---
+++ src/components/UI/table/pagination.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/navigation";
+import Button from "../button";
+import { cn } from "@/shared/utils/styling";
+
+type TProps = {
+  currentPage: number;
+  routeBase: string;
+  pagesList: (number | null)[];
+};
+
+export const Pagination = ({ currentPage, routeBase, pagesList }: TProps) => {
+  const router = useRouter();
+  return (
+    <div className="flex justify-center mt-4 mb-10 border-t border-gray-200 pt-4 gap-4">
+      {pagesList.map((item, idx) =>
+        item === null ? (
+          <span key={`${item}-${idx}`}>...</span>
+        ) : (
+          <Button
+            key={`${item}-${idx}`}
+            onClick={() => router.push(`${routeBase}${item}`)}
+            disabled={item === currentPage}
+            className={cn({
+              "border-none bg-white": item === null,
+              "opacity-50": item !== currentPage,
+            })}
+          >
+            {item}
+          </Button>
+        )
+      )}
+    </div>
+  );
+};
