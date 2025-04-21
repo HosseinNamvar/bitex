ed35f92b5a9eb95671af06955b72360f548bbb3b
HosseinNamvar <hossein_nam@yahoo.com>
2025-04-21 12:21:40 +0200

    refactor: moved sort list data to product list domain

--- src/domains/store/productList/constants/index.ts
+++ src/domains/store/productList/constants/index.ts
@@ -1,4 +1,5 @@
 import { TListSort } from "@/types/list";
+import { TDropDown } from "@/types/uiElements";
 
 import { TFilters } from "../types";
 
@@ -15,4 +16,27 @@ export const SORT_DATA: TListSort[] = [
   { sortName: "price", sortType: "desc" },
   { sortName: "price", sortType: "asc" },
   { sortName: "name", sortType: "asc" },
+];
+
+export const sortDropdownData: TDropDown[] = [
+  {
+    text: "Newest",
+    value: "0",
+  },
+  {
+    text: "Oldest",
+    value: "1",
+  },
+  {
+    text: "Most Expensive",
+    value: "2",
+  },
+  {
+    text: "Cheapest",
+    value: "3",
+  },
+  {
+    text: "Name",
+    value: "4",
+  },
 ];
